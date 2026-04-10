3cbf5b1fa170b189994fb5740f756150e0a7259c
Arnav Sukhija <arnavsonic@gmail.com>
2026-04-10 10:08:26 +0200

    remove worker cuda fix

--- actsafe/rl/episodic_async_env.py
+++ actsafe/rl/episodic_async_env.py
@@ -187,12 +187,6 @@ class EpisodicAsync:
 
 
 def _worker(ctor, conn, time_limit, action_repeat):
-    # CRITICAL FIX: Hide the GPU from the worker subprocesses!
-    # Otherwise, when cloudpickle unpickles the environment, it imports JAX in the
-    # child process, which grabs the GPU driver lock. When the parent process then
-    # tries to compile PTX to SASS, it blocks infinitely on the driver lock.
-    import os
-    os.environ["CUDA_VISIBLE_DEVICES"] = ""
 
     try:
         env = TimeLimit(cloudpickle.loads(ctor)(), time_limit)
